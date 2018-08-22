dc6580c96eb7266ad2c42f828d4335c4ad649931
brianbentancourt <brianbentancourt9@gmail.com>
2018-08-22 20:16:59 -0300

    imprimiendo datos de estado

--- src/entries/redux.jsx
+++ src/entries/redux.jsx
@@ -29,3 +29,11 @@ const store = createStore(
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
+
+const $container = document.getElementById('playlist');
+const playlist = store.getState();
+playlist.forEach((item)=>{
+  const template = document.createElement('p');
+  template.textContent = item.title;
+  $container.appendChild(template);
+})
